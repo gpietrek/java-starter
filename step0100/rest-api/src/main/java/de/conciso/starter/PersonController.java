8d4a577593ca24616b1fec8c99a8303c92c64be7
Christoph Rogalla <christoph.rogalla001@stud.fh-dortmund.de>
2024-08-30 14:23:55 +0200

    add CrossOrigin

--- step0100/rest-api/src/main/java/de/conciso/starter/PersonController.java
+++ step0100/rest-api/src/main/java/de/conciso/starter/PersonController.java
@@ -2,14 +2,10 @@ package de.conciso.starter;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:4200")
 @RequestMapping("/api/person")
 public class PersonController {
 
