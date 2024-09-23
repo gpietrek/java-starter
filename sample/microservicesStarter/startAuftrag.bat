b820b5c06847c4612dcb3270b2ed3a97625ea0bd
Chrisy455 <110563780+Chrisy455@users.noreply.github.com>
2024-09-23 19:41:37 +0200

    Deleted Starter
    Delete sample/microservicesStarter directory

--- sample/microservicesStarter/startAuftrag.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\chris\Desktop\Uni\Projektarbeit II\java-starter\sample\auftrag
-cmd.exe /k "mvn clean install docker:run -Pdocker"
--- sample/microservicesStarter/starter.bat
+++
@@ -1,9 +0,0 @@
-start cmd.exe /k "startPerson.bat"
-
-timeout /t 5 /nobreak
-
-start cmd.exe /k "startAuftrag.bat"
-
-timeout /t 5 /nobreak
-
-start cmd.exe /k "startShop.bat"
