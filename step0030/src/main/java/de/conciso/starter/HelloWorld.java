a8691a011f2f0b1f185bb28d4427b3255b7e66a7
Georg Pietrek <georg.pietrek@conciso.de>
2025-03-23 10:51:21 +0100

    replace log4j with logback

--- step0030/src/main/java/de/conciso/starter/HelloWorld.java
+++ step0030/src/main/java/de/conciso/starter/HelloWorld.java
@@ -1,11 +1,11 @@
 package de.conciso.starter;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HelloWorld {
 
-  private static Logger logger = LogManager.getLogger(HelloWorld.class);
+  private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 
   public static void main(String[] args) {
     logger.info("Hello World!");
